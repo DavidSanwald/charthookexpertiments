3aa2b19379e17607da8a7cea41050f84c2b8ce40
DavidSanwald <david.sanwald@gmail.com>
2018-11-03 11:50:15 +0100

    yay

--- src/App.js
+++ src/App.js
@@ -1,28 +1,117 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, {
+  useEffect,
+  useState,
+  useRef,
+  useLayoutEffect,
+  forwardRef,
+  useMemo
+} from 'react'
+import { Layout, Card } from 'antd'
+import * as d3 from 'd3'
+import * as R from 'ramda'
+import { DateTime } from 'luxon'
+import rawData from './dummyData'
+const { Header, Content, Footer, Sider } = Layout
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const margin = { top: 20, right: 10, bottom: 20, left: 10 }
+const outerHeight = 300
+const outerWidth = 900
+const width = outerWidth - margin.left - margin.right
+const height = outerHeight - margin.bottom - margin.top
+const x = R.prop('acres')
+const y = R.prop('vertical')
+const keyAcc = R.prop('resort_name')
+const xExtent = d3.extent(rawData, x)
+const yExtent = d3.extent(rawData, y)
+
+const Scatter = ({ x, y, xScale, yScale, keyAcc, data, size }) => {
+  console.log(size)
+  const xAcc = R.compose(
+    xScale,
+    x
+  )
+  const yAcc = R.compose(
+    yScale,
+    y
+  )
+  const isSelected = isInBounds(size)
+  const circles = data.map(datum => (
+    <circle
+      key={`dot-${keyAcc(datum)}`}
+      fill={isSelected(xAcc(datum), yAcc(datum)) ? 'red' : 'green'}
+      className="dot"
+      r="3.5"
+      cx={xAcc(datum)}
+      cy={yAcc(datum)}
+    />
+  ))
+  return <g>{circles}</g>
 }
 
-export default App;
+const useBrush = (ref, size, setSize) => {
+  const brush = d3
+    .brush()
+    .extent([[0, 0], [width, height]])
+    .on('brush', function brushed() {
+      const s = d3.event.selection
+      setSize(s)
+    })
+  useEffect(() => {
+    const container = d3
+      .select(ref.current)
+      .selectAll('.container')
+      .data([null])
+    container
+      .enter()
+      .append('g')
+      .merge(container)
+      .call(brush)
+  })
+  return size
+}
+const isInBounds = s => (x, y) => {
+  const x0 = s[0][0]
+  const y0 = s[0][1]
+  const dx = s[1][0] - x0
+  const dy = s[1][1] - y0
+  return x >= x0 && x <= x0 + dx && y >= y0 && y <= y0 + dy
+}
+const App = () => {
+  const ref = useRef(null)
+  const [size, setSize] = useState([[0, 0], [width, height]])
+  useBrush(ref, size, setSize)
+  const xScale = d3
+    .scaleLinear()
+    .domain(xExtent)
+    .range([0, width])
+  const yScale = d3
+    .scaleLinear()
+    .domain(yExtent)
+    .range([height, 0])
+  
+  return (
+    <Layout>
+      <Content>
+        <Card title="Chart">
+          <svg width={outerWidth} ref={ref} height={outerHeight}>
+            <g
+              className="container"
+              transform={`translate(${margin.left}, ${margin.top})`}>
+              <Scatter
+                size={size}
+                data={rawData}
+                keyAcc={keyAcc}
+                y={y}
+                x={x}
+                yScale={yScale}
+                xScale={xScale}
+              />
+            </g>
+          </svg>
+        </Card>
+      </Content>
+    </Layout>
+  )
+}
+export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import 'resize-observer-polyfill'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+loadPolyfills().then(() =>
+  ReactDOM.render(<App />, document.getElementById('root'))
+)
+
+/**
+ * Do feature detection, to figure out which polyfills needs to be imported.
+ **/
+function loadPolyfills() {
+  const polyfills = []
+
+  if (!supportsResizeObserver()) {
+    polyfills.push(import('resize-observer-polyfill'))
+  }
+
+  return Promise.all(polyfills)
+}
+
+function supportsResizeObserver() {
+  return (
+    'ResizeObserver' in global &&
+    'ResizeObserverEntry' in global &&
+    'contentRect' in ResizeObserverEntry.prototype
+  )
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
